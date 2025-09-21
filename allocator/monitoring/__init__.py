84a26325becf94651ef23c89227b0aaa3e7da989
myvps2gh <github.gauging673@slmails.com>
2025-09-21 12:22:05 -0400

    fix init issue

--- allocator/monitoring/__init__.py
+++ allocator/monitoring/__init__.py
@@ -4,10 +4,8 @@ Monitoring and mempool watching for Allocator AI
 
 from .mempool_watcher import MempoolWatcher
 from .trade_parser import TradeParser
-from .performance_monitor import PerformanceMonitor
 
 __all__ = [
     "MempoolWatcher",
-    "TradeParser", 
-    "PerformanceMonitor"
+    "TradeParser"
 ]
--- allocator/web/__init__.py
+++ allocator/web/__init__.py
@@ -3,9 +3,7 @@ Web interface and API for Allocator AI
 """
 
 from .dashboard import create_app
-from .api import create_api_blueprint
 
 __all__ = [
-    "create_app",
-    "create_api_blueprint"
+    "create_app"
 ]
